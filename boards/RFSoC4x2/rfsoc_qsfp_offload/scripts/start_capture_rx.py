529c7a7ea81c0552e81544e71399a82ad351a022
Nicholas Rainville <nick.rainville@gmail.com>
2025-05-22 10:28:28 -0600

    10G Ethernet streaming enabled with Xilinx XXV Ethernet core (#6)
    - Streams broadcast UDP packets with RF ADC data from "ADC_B" input on RFSoC 4x2 board
    - Example capture script start_capture_rx.py, bitfile path needs to be updated to point to bitstream/rfsoc_offload_10g.bit
    - Example notebook rfsoc_offload_10g_adc.ipynb
    
    * 10gbe debug
    
    * flipped order of fifo and data width converter
    
    * Path fix
    
    * udp stream verilog example
    
    * UDP stream debug
    
    * UDP dummy stream, working example. Includes python notebook for testing
    
    * First pass 10g streaming
    
    * FIFO working in simulation
    
    * working adc to udp stream, with gaps
    
    * Working stream with 64-bit ocunter
    
    * Block design for working 76.8MHz example
    
    * ADC stream working without gaps
    
    * RF ADC to 10G NIC streaming, tested working with GNU Radio
    
    ---------
    
    Co-authored-by: nrainvil <nicholas.rainville@colorado.edu>

--- boards/RFSoC4x2/rfsoc_qsfp_offload/scripts/start_capture_rx.py
+++ boards/RFSoC4x2/rfsoc_qsfp_offload/scripts/start_capture_rx.py
@@ -2,6 +2,8 @@ import time
 import signal
 import sys
 import argparse
+import xrfdc
+import xrfclk
 from rfsoc_qsfp_offload.overlay import Overlay
 
 global exit_flag
@@ -14,67 +16,53 @@ def signal_handler(sig, frame):
     
 def main(args):
     
-    # Correct for ADC ordering
-    if(args.channel == 0):
-        adc_block = 1
-    elif(args.channel == 1):
-        adc_block = 0
-    else:
-        print("Invalid ADC Channel")
-        return
-
+    adc_channel = 0
     f_c = args.freq
-    print("Starting RF capture on ADC%d at %0.3fMHz" % (args.channel, f_c))
+    print("Starting RF capture on ADC%d at %0.3fMHz" % (adc_channel, f_c))
 
     board_ip = '192.168.4.99'
     client_ip = '192.168.4.1'
 
-    print("Initializing RFSoC QSFP Offload Overlay")
+    print("Initializing RFSoC 10G Overlay")
     #ol = Overlay(ignore_version=True)
-    ol = Overlay(bitfile_name="/opt/git/rfsoc_qsfp_10g/boards/RFSoC4x2/rfsoc_qsfp_offload/bitstream/rfsoc_offload_10g.bit", ignore_version=True)
+    ol = Overlay(bitfile_name="/opt/git/rfsoc_qsfp_10g/boards/RFSoC4x2/rfsoc_qsfp_offload/bitstream/rfsoc_offload_10g_adc0.bit", ignore_version=True)
     # Wait for overlay to initialize
-    ol.cmac.mmio.write(0x107C, 0x3) # RSFEC_CONFIG_ENABLE
-    ol.cmac.mmio.write(0x1000, 0x7) # RSFEC_CONFIG_INDICATION_CORRECTION
     time.sleep(5) # Magic sleep
 
-    ol.cmac.start()
-    res = ol.netlayer.set_ip_address(board_ip, debug=True)
-
-    print("Network confguration complete IP: %s" % (res['inet addr']))
-
-    ol.netlayer.sockets[0] = (client_ip, 60133, 60133, True)
-    ol.netlayer.populate_socket_table()
-    ol.adc_select(adc_block) # Select RF-ADC source for packets
-
-    ADC_TILE = 2       # ADC Tile 226
-    ADC_SAMPLE_FREQUENCY = 1024  # MSps
-    ADC_PLL_FREQUENCY    = 491.52   # MHz
-    ADC_FC = -1*f_c # Tune to center frequency
-
-    # Stop UDP stream if running
-    ol.enable_udp(False)
+    print("Network confguration complete IP: %s" % (board_ip))
 
     # Start ADC
-    ol.initialise_adc(tile=ADC_TILE,
-                    block=adc_block,
-                    pll_freq=ADC_PLL_FREQUENCY,
-                    fs=ADC_SAMPLE_FREQUENCY,
-                    fc=ADC_FC)
+    ADC_TILE = 2       # ADC Tile 226
+    ADC_BLOCK = 0       # ADC Block 0
+    ADC_SAMPLE_FREQUENCY = 1024  # MSps
+    ADC_PLL_FREQUENCY    = 491.52   # MHz
+    ADC_FC = -1*f_c # FM Band
 
-    # Decimate by (16x)
-    ol.set_decimation(tile=ADC_TILE,block=adc_block,sample_rate=64e6)
+    pll_freq = ADC_PLL_FREQUENCY
+    fs = ADC_SAMPLE_FREQUENCY
+    tile = ADC_TILE
+    block=ADC_BLOCK
+    fc = ADC_FC
 
-    # Set packet size
-    ol.enable_udp(True)
+    ol.rfdc.adc_tiles[tile].DynamicPLLConfig(1, pll_freq, fs)
+    ol.rfdc.adc_tiles[tile].blocks[block].NyquistZone = 1
+    ol.rfdc.adc_tiles[tile].blocks[block].MixerSettings = {
+                'CoarseMixFreq':  xrfdc.COARSE_MIX_BYPASS,
+                'EventSource':    xrfdc.EVNT_SRC_TILE,
+                'FineMixerScale': xrfdc.MIXER_SCALE_1P0,
+                'Freq':           fc,
+                'MixerMode':      xrfdc.MIXER_MODE_R2C,
+                'MixerType':      xrfdc.MIXER_TYPE_FINE,
+                'PhaseOffset':    0.0
+            }
+    ol.rfdc.adc_tiles[tile].blocks[block].UpdateEvent(xrfdc.EVENT_MIXER)
+    ol.rfdc.adc_tiles[tile].SetupFIFO(True)
 
     print("Starting UDP stream")
     print("Ctrl-C to exit")
     while(not exit_flag):
         time.sleep(1)
         print(".", end='', flush=True)
-
-    # Stop packet generator
-    ol.enable_udp(False)
 
 if __name__ == "__main__":
     # CTRL-C handler
@@ -88,8 +76,6 @@ if __name__ == "__main__":
         )
     parser.add_argument('-f', '--freq',type=float,help='Center frequency (MHz)',
                         default = '1000')
-    parser.add_argument('-c', '--channel',type=int,help='ADC Channel',
-                        default = '0')
                         
     args = parser.parse_args()
     main(args)
