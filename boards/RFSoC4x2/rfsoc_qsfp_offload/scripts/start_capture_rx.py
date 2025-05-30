c462dd86382d0d7a5484e62bcd1add75694b9daa
nrainvil <nicholas.rainville@colorado.edu>
2025-05-30 07:54:21 -0600

    Capture script update, selectable channels, bitfile

--- boards/RFSoC4x2/rfsoc_qsfp_offload/scripts/start_capture_rx.py
+++ boards/RFSoC4x2/rfsoc_qsfp_offload/scripts/start_capture_rx.py
@@ -16,9 +16,8 @@ def signal_handler(sig, frame):
     
 def main(args):
     
-    adc_channel = 0
     f_c = args.freq
-    print("Starting RF capture on ADC%d at %0.3fMHz" % (adc_channel, f_c))
+    print(f"Starting RF capture on ADC Channel {args.channels} at {f_c:0.3f} MHz") 
 
     board_ip = '192.168.4.99'
     client_ip = '192.168.4.1'
@@ -28,8 +27,8 @@ def main(args):
 
     # Wait for overlay to initialize
     time.sleep(5) # Magic sleep
-
-    print("Network confguration complete IP: %s" % (board_ip))
+    ol.adc_to_udp_stream_A.register_map.USER_RESET = 1
+    ol.adc_to_udp_stream_B.register_map.USER_RESET = 1
 
     # Start ADC
     ADC_TILE = 2       # ADC Tile 226
@@ -42,57 +41,60 @@ def main(args):
     tile = ADC_TILE
     fc = ADC_FC
 
-    block = 1       # ADC Block 1 (A)
-    ol.rfdc.adc_tiles[tile].DynamicPLLConfig(1, pll_freq, fs)
-    ol.rfdc.adc_tiles[tile].blocks[block].NyquistZone = 1
-    ol.rfdc.adc_tiles[tile].blocks[block].MixerSettings = {
-                'CoarseMixFreq':  xrfdc.COARSE_MIX_BYPASS,
-                'EventSource':    xrfdc.EVNT_SRC_TILE,
-                'FineMixerScale': xrfdc.MIXER_SCALE_1P0,
-                'Freq':           fc,
-                'MixerMode':      xrfdc.MIXER_MODE_R2C,
-                'MixerType':      xrfdc.MIXER_TYPE_FINE,
-                'PhaseOffset':    0.0
+    mixer_settings = {
+            'CoarseMixFreq':  xrfdc.COARSE_MIX_BYPASS,
+            'EventSource':    xrfdc.EVNT_SRC_TILE,
+            'FineMixerScale': xrfdc.MIXER_SCALE_1P0,
+            'Freq':           fc,
+            'MixerMode':      xrfdc.MIXER_MODE_R2C,
+            'MixerType':      xrfdc.MIXER_TYPE_FINE,
+            'PhaseOffset':    0.0
             }
-    ol.rfdc.adc_tiles[tile].blocks[block].UpdateEvent(xrfdc.EVENT_MIXER)
-    ol.rfdc.adc_tiles[tile].SetupFIFO(True)
 
     block = 0       # ADC Block 0 (B)
     ol.rfdc.adc_tiles[tile].DynamicPLLConfig(1, pll_freq, fs)
     ol.rfdc.adc_tiles[tile].blocks[block].NyquistZone = 1
-    ol.rfdc.adc_tiles[tile].blocks[block].MixerSettings = {
-                'CoarseMixFreq':  xrfdc.COARSE_MIX_BYPASS,
-                'EventSource':    xrfdc.EVNT_SRC_TILE,
-                'FineMixerScale': xrfdc.MIXER_SCALE_1P0,
-                'Freq':           fc,
-                'MixerMode':      xrfdc.MIXER_MODE_R2C,
-                'MixerType':      xrfdc.MIXER_TYPE_FINE,
-                'PhaseOffset':    0.0
-            }
+    ol.rfdc.adc_tiles[tile].blocks[block].MixerSettings = mixer_settings
+    ol.rfdc.adc_tiles[tile].blocks[block].UpdateEvent(xrfdc.EVENT_MIXER)
+    ol.rfdc.adc_tiles[tile].SetupFIFO(True)
+
+    block = 1       # ADC Block 1 (A)
+    ol.rfdc.adc_tiles[tile].DynamicPLLConfig(1, pll_freq, fs)
+    ol.rfdc.adc_tiles[tile].blocks[block].NyquistZone = 1
+    ol.rfdc.adc_tiles[tile].blocks[block].MixerSettings = mixer_settings
     ol.rfdc.adc_tiles[tile].blocks[block].UpdateEvent(xrfdc.EVENT_MIXER)
     ol.rfdc.adc_tiles[tile].SetupFIFO(True)
 
     print(f"Starting UDP stream on: {args.channels}")
-    ol.adc_to_udp_stream_A.register_map.USER_RESET = 0
-    ol.adc_to_udp_stream_B.register_map.USER_RESET = 0
+    if 'A' in args.channels:
+        ol.adc_to_udp_stream_A.register_map.USER_RESET = 0
+    if 'B' in args.channels:
+        ol.adc_to_udp_stream_B.register_map.USER_RESET = 0
 
     print("Ctrl-C to exit")
     while(not exit_flag):
         time.sleep(1)
         print(".", end='', flush=True)
 
-
     print("Stopping UDP stream")
-    if 'A' in args.channels:
-        ol.adc_to_udp_stream_A.register_map.USER_RESET = 0
-    if 'B' in args.channels:
-        ol.adc_to_udp_stream_B.register_map.USER_RESET = 0
+    ol.adc_to_udp_stream_A.register_map.USER_RESET = 1
+    ol.adc_to_udp_stream_B.register_map.USER_RESET = 1
 
 if __name__ == "__main__":
     # CTRL-C handler
     global exit_flag 
     exit_flag = False
-    signal.signal(signal.SIGINT, signal_handler)
+
+    termination_signals = [
+        signal.SIGINT,   # Ctrl+C
+        signal.SIGTERM,  # Termination (kill)
+        signal.SIGHUP,   # Hangup
+        signal.SIGQUIT,  # Ctrl+\
+        signal.SIGABRT,  # Aborted
+    ]
+
+    for sig in termination_signals:
+        signal.signal(sig, signal_handler)
 
     parser = argparse.ArgumentParser(
         description='Tune RFSoC and stream data over QSFP',
@@ -100,7 +102,7 @@ if __name__ == "__main__":
         )
     parser.add_argument('-f', '--freq',type=float,help='Center frequency (MHz)',
                         default = '1000')
-    parser.add_argument('-c', '--channels',type=str,nargs='+',
+    parser.add_argument('-c', '--channels',type=str,nargs='*',
                         choices=['A', 'B', 'C', 'D'], help='List of channels (A, B)',
                         default = 'A')
                         
